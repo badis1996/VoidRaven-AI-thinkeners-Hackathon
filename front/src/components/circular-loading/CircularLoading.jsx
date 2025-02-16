4cc6d682242bdb864e736585de9b63066a9876b2
Khaled Abdrabo <khaledm@rocketmail.com>
2025-02-16 17:03:49 +0100

    Vasy

--- front/src/components/circular-loading/CircularLoading.jsx
+++ front/src/components/circular-loading/CircularLoading.jsx
@@ -1,14 +1,18 @@
 import './../../assets/css/circular-loading.css';
+import './../../assets/css/style.css';
+
 
 const CircularLoading = () => {
     return (
-        <div className="circular-loading-root">
-            <span className="circular-loading-progressbar">
-                <svg className="circular-loading-svg" viewBox="22 22 44 44">
-                    <circle className="circular-loading-circle" cx="44" cy="44" r="20.2" fill="none" strokeWidth="3.6">
-                    </circle>
-                </svg>
-            </span>
+        <div className="center-to-screen">
+            <div className="circular-loading-root">
+                <span className="circular-loading-progressbar">
+                    <svg className="circular-loading-svg" viewBox="22 22 44 44">
+                        <circle className="circular-loading-circle" cx="44" cy="44" r="20.2" fill="none" strokeWidth="3.6">
+                        </circle>
+                    </svg>
+                </span>
+            </div>
         </div>
     );
 }
